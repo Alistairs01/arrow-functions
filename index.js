66692e54552edc9f7b72e09bec695ab19c6b72b1
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-03-24 19:35:51 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// create an arrow function expression called divide 
+// it divides 2000 by 100
+// expect (divide(a,b)).to.equal(a/b)
+const divide = (a, b) => a / b 
+let a = 2000;
+let b = 100;
+// create an arrow function called square
+// square multiplys the number x by its self
+// let x = 2
+// expect (square(x)).to.equal(4) 
+const square = (x) => x * x
+let x = 2;
+// create a function called add
+// adds two numbers c and d together
+//let (c=3) and (d=4)
+// expect (add(c,d)).to.equal(a+b)
+const add = (c, d) => c + d
+let c = 3;
+let d = 4;
